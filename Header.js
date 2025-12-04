5e6423e3d8efee2d3fdfe0573798a78051d3b166
Carlos11378 <litteralinea@gmail.com>
2025-12-04 14:33:21 -0300

    Add Header component with navigation

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Meu App Literário</h1>
+      <nav>
+        <a href="/">Início</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
